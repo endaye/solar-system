e1d9f5e3de2ef10af179be2bfc599d22dab7f0eb
Yuancheng Zhang <old.far.hot@gmail.com>
2017-03-28 11:29:52 -0500

    add main.cpp

--- solar-system/main.cpp
+++ solar-system/main.cpp
@@ -1,5 +1,5 @@
 //
-//  solarsystem.cpp
+//  main.cpp
 //  solar-system
 //
 //  Created by Yuancheng Zhang on 3/28/17.
@@ -7,6 +7,19 @@
 //
 
 #include "solarsystem.hpp"
+
+// 使用 GLUT 的基本头文件
+#ifndef _glut_
+#define _glut_
+#include <GLUT/glut.h>
+#endif
+
+// 创建图形窗口的基本宏
+#define WINDOW_X_POS 50
+#define WINDOW_Y_POS 50
+#define WIDTH 700
+#define HEIGHT 700
+
 
 SolarSystem solarsystem;
 
@@ -48,19 +61,19 @@ int main(int argc, char*  argv[])
     // 这是一个回调函数，每当 GLUT 确定一个窗口的内容需要更新显示的时候,
     // glutDisplayFunc 注册的回调函数就会被执行.
     glutDisplayFunc(onDisplay);
-
+    
     // glutIdleFunc(void (*func)(void)) 将指定一个函数，用于处理当事件循环
     // 处于空闲的时候，就执行这个函数。这个回调函数接受一个函数指针作为它的唯一参数
     glutIdleFunc(onUpdate);
-
+    
     // glutKeyboardFunc(void (*func)(unsigned char key, int x, int y))
     // 会将键盘上的键与一个函数关联，当这个键被按下或者释放时，函数就会调用
     glutKeyboardFunc(onKeyboard);
     
     // 因此下面的三行实际上是在向 GLUT 注册关键的三个回调函数
-//    glutDisplayFunc(onDisplay);
-//    glutIdleFunc(onUpdate);
-//    glutKeyboardFunc(onKeyboard);
+    //    glutDisplayFunc(onDisplay);
+    //    glutIdleFunc(onUpdate);
+    //    glutKeyboardFunc(onKeyboard);
     
     glutMainLoop();
     return 0;
